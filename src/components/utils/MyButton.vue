963d3441a260f51e59766633033cc4b857a599b0
falep9 <felipemotta1515@gmail.com>
2026-04-28 17:06:34 -0300

    Componentes base MyButton e MyCard

---
+++ src/components/utils/MyButton.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  label: String,
+  tipo: {
+    type: String,
+    default: 'primary'
+  }
+})
+</script>
+
+<template>
+  <button :class="['btn', tipo]">
+    {{ label }}
+  </button>
+</template>
+
+<style scoped>
+.btn {
+  padding: 10px 10px;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  font-weight: bold;
+  width: 5%;
+}
+.primary { background-color: #42b983; color: white; }
+.secondary { background-color: #35495e; color: white; }
+.danger { background-color: #e74c3c; color: white; }
+</style>
---
+++ src/components/utils/MyCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="AppCard">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.AppCard {
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 16px;
+  margin: 8px;
+}
+
+</style>
